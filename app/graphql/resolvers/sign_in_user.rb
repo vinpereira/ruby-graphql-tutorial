4d6129e97085e3d0025cf1391061f684c71d6451
Vinícius Pereira <jedipereira@gmail.com>
2018-01-20 16:10:28 -0200

    add session to store token for requests

--- app/graphql/resolvers/sign_in_user.rb
+++ app/graphql/resolvers/sign_in_user.rb
@@ -9,7 +9,7 @@ class Resolvers::SignInUser < GraphQL::Function
       field :user, Types::UserType
     end
   
-    def call(_obj, args, _ctx)
+    def call(_obj, args, ctx)
       input = args[:email]
   
       # basic validation
@@ -17,14 +17,15 @@ class Resolvers::SignInUser < GraphQL::Function
   
       user = User.find_by email: input[:email]
   
-      # ensures we have the correct user
+      # ensures it is the correct user
       return unless user
       return unless user.authenticate(input[:password])
   
-      # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
       token = crypt.encrypt_and_sign("user-id:#{ user.id }")
   
+      ctx[:session][:token] = token
+
       OpenStruct.new({
         user: user,
         token: token
